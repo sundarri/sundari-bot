56c1599eaf5d13831cf6c74a3e9c6fdb1e908b5b
sundarri <151605405+sundarri@users.noreply.github.com>
2023-11-21 16:04:06 +0530

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import getenv
 
 class Config(object):
-      API_HASH = getenv("API_HASH")
-      API_ID = int(getenv("API_ID", 0))      
-      BOT_TOKEN = getenv("BOT_TOKEN", "")
+      API_HASH = getenv("21562655")
+      API_ID = int(getenv("82a353e4cf209ae679b537c1fff17c9c", 0))      
+      BOT_TOKEN = getenv("6820240200:AAGBVWRJJyn82CYv8R6mzJFQtPm8APKCjjU", "")
       CHANNEL = list(x for x in getenv("CHANNEL_ID", "").replace("\n", " ").split(' '))
